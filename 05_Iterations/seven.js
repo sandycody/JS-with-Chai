e21606bbe9d9de49ba33fb2315f081c86d8666a4
Sandeep Wadhawan <68498988+sandycody@users.noreply.github.com>
2024-02-06 19:40:49 +0000

    Thoda Aur kaam kar kia

--- 05_Iterations/seven.js
+++ 05_Iterations/seven.js
@@ -1,1 +1,9 @@
 // Map function in JS
+
+const arr = [34 ,67, 10, 3, 5, 665];
+const mapMine = arr
+                .map((num) => num + 10)
+                .map( (num) => num * 2)
+                .filter( (num => num >= 50));
+
+console.log(mapMine);
