7a8fe0ebf53d2177c8b37aa0151426344c78bf85
Sandeep <sandeepwadhawan2001@gmail.com>
2024-03-04 14:27:48 +0530

    static keyword in JS is done

---
+++ 14_OOPS/staticProp.js
@@ -0,0 +1,54 @@
+/* static keyword in JS */
+// 1. static keyword defines static methods for classes.
+
+// 2. Static methods are called directly on the class (Car from the example below) without creating an instance/object (myCar) of the class.
+
+// Example :
+
+/*
+class Car {
+  constructor(brand) {
+    this.carname = brand;
+  }
+  static hello() {  // static method
+    return "Hello!!";
+  }
+}
+
+const mycar = new Car("Ford"); 
+*/
+
+class User {
+    constructor(username) {
+        this.username = username;
+    }
+
+    logMe() {
+        console.log(`USERNAME: ${this.username}`);
+    }
+
+    // Jase ki ek User create ho, tabhi hi uss User ko ek unique Id mil jaye
+    static createId() {
+        return `123`;
+    }
+}
+
+const user = new User("Sandeep");
+// console.log(user.createId()); //Error => user,createId is not a function
+console.log(User.createId()); //123
+
+// Another Class which extends User
+class Teacher extends User {
+    constructor(username, email, password) {
+        super(username);
+        this.email = email;
+        this.password = password;
+    }
+}
+
+const gadget = new Teacher("laptop", "Mi@intel.com", "4321");
+
+gadget.logMe(); //USERNAME: laptop
+// console.log(gadget.createId()); //TypeError: gadget.createId is not a function
+
+console.log(Teacher.createId()); //123
